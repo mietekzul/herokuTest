a97917d325f53a95bf6e56e3fba5bd111c6e902b
mietekzul <noname_18@wp.pl>
2017-04-09 12:45:53 +0200

    First commit.

---
+++ src/main/java/CloudHerokuApp.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class CloudHerokuApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudHerokuApp.class, args);
+	}
+}
